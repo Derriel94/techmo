05d8cf125e3e7fae6399fad8f0568bbe95595655
Derriel <derrielcollins96@gmail.com>
2021-11-17 12:53:10 -0600

    playing with background for homepage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,14 @@
-
+import './blue.png';
+import './toriko.png';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>TechMo</h1>
+      <div class="toriko"></div>
+      <div class="blue"></div>
+      
 
     </div>
   );
