4a6a5766e2b266726961952c008ac22d0e9640d2
Derriel <derrielcollins96@gmail.com>
2021-11-19 12:55:44 -0600

    working on animation for homepage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,22 @@
-import './blue.png';
-import './toriko.png';
 import './App.css';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+
 
 function App() {
+  const [state, toggle] = useState(true);
+  const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 1000 } });
   return (
-    <div>
-      <h1>TechMo</h1>
-      <div class="toriko"></div>
-      <div class="blue"></div>
+    <div class="background">
       
-
+      <div onClick={() => toggle(!state)}>
+      <animated.div
+        style={{
+     fontSize: 50,
+        }}>
+        <h1 class="title">TechMo</h1>
+      </animated.div>
+    </div>
     </div>
   );
 }
